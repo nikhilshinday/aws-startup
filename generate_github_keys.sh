715b2a469d4ea1f011b4cb4f431e0962de36a8bb
Nikhil Shinday <nikhilshinday@gmail.com>
2017-06-21 19:27:21 -0400

    making sure it works as an executable

--- generate_github_keys.sh
+++ generate_github_keys.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 ssh-keygen -t rsa -b 4096 -C "nikhilshinday@github.com"
 eval "$(ssh-agent -s)"
 echo "copy/paste this into repository ssh permissions:" 
--- run.sh
+++ run.sh
@@ -1,9 +1,8 @@
-./sys_pkg.sh
+#!/bin/bash
 
+./sys_pkg.sh
 virtualenv py2
 source py2/bin/activate
 pip install -r requirements.txt
-
 ./geopredict_shortcuts.sh
-
 ./generate_github_keys.sh
