93ffd3841afde3825cd1c95b35732a1bc5e1df9e
Nikhil Shinday <nikhilshinday@gmail.com>
2017-06-21 19:45:56 -0400

    silence prompts

---
+++ paste_in_aws.sh
@@ -0,0 +1,6 @@
+sudo apt-get install git 
+git clone https://github.com/nikhilshinday/aws-startup.git
+cd aws-startup
+chmod 777 *.sh
+./run.sh
+cd ..
--- sys_pkg.sh
+++ sys_pkg.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-sudo apt update
-sudo apt upgrade
-sudo apt-get install libmysqlclient-dev
-sudo apt-get install zip 
+sudo apt update --assume-yes
+sudo apt upgrade --assume-yes
+sudo apt-get install --assume-yes libmysqlclient-dev
+sudo apt-get install --assume-yes zip 
 # NFS for remote file directory
-sudo apt-get install nfs-common
+sudo apt-get install --assume-yes nfs-common
 # Python things 
-sudo apt-get install python-dev
-sudo apt-get install python-pip 
-sudo apt-get install python-tk
-sudo apt-get install ipython
-sudo apt-get install virtualenv
+sudo apt-get install --assume-yes python-dev
+sudo apt-get install --assume-yes python-pip 
+sudo apt-get install --assume-yes python-tk
+sudo apt-get install --assume-yes ipython
+sudo apt-get install --assume-yes virtualenv
 
