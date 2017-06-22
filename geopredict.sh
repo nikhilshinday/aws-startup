f5c62bf6caff217f16ef61d595fe2cd93f7e11c6
Nikhil Shinday <nikhilshinday@gmail.com>
2017-06-22 12:18:07 -0400

    renaming and python code debugging

--- geopredict.sh
+++ geopredict.sh
@@ -1,7 +1,8 @@
+# GeoPredict-Specific Shortcuts
 pushd . 
 cd ~
-echo "alias mysqlgp=\"mysql --host=geopredict.cxo0yl5doawm.us-east-1.rds.amazonaws.com --port=3306 --user=geopredict --password=ground2017\"" >> .bashrc 
 # connect the nsf server if it's not restarted already:
 # sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 fs-7666ba3f.efs.us-east-1.amazonaws.com:/ efs
+echo "alias mysqlgp=\"mysql --host=geopredict.cxo0yl5doawm.us-east-1.rds.amazonaws.com --port=3306 --user=geopredict --password=ground2017\"" >> .bashrc 
 echo "source py2/bin/activate" >> .bashrc
 popd 
--- run.sh
+++ run.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
-./apt-install-packages.sh
-
-pushd . 
-cd ~
-virtualenv py2
-source py2/bin/activate
-pip install -r requirements.txt
-deactivate 
-popd 
-./geopredict_shortcuts.sh
-./generate_github_keys.sh
+./install.sh
+./python.sh
+./efs_mount.sh
+./geopredict.sh
+./github_keys.sh
