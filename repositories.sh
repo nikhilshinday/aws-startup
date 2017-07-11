d2062c588a6f5d91edfa1a90000fb936c245ccbd
Nikhil Shinday <nikhilshinday@gmail.com>
2017-07-11 12:03:37 -0400

    testing something

--- repositories.sh
+++ repositories.sh
@@ -2,6 +2,6 @@
 pushd . 
 cd ~
 cp efs/aws_config/ssh/* ~/.ssh/id_rsa
-# ssh-keyscan github.com:nikhilshinday/GeoPredict.git >> ~/.ssh/known_hosts
+ssh-keyscan github.com:nikhilshinday/GeoPredict.git >> ~/.ssh/known_hosts
 git clone https://github.com:nikhilshinday/GeoPredict.git
 popd 
